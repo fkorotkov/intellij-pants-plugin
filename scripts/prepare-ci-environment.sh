b555bf22fc5e4ce4712d7c747c9dae226c56a390
fkorotkov <fkorotkov@twitter.com>
2015-05-17 14:17:11 -0700

    cached IJ and plugins between Travis CI runs

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -2,8 +2,8 @@
 
 prepare_ci_env() {
   CWD=$(pwd)
-  export INTELLIJ_PLUGINS_HOME="$CWD/.cache/intellij/plugins"
-  export OSS_PANTS_HOME="$CWD/.cache/intellij/pants"
-  export INTELLIJ_HOME="$CWD/.cache/intellij/idea-dist"
-  export JDK_LIBS_HOME="$CWD/.cache/intellij/jdk-libs"
+  export INTELLIJ_PLUGINS_HOME="$CWD/.cache/intellij/$IJ_BUILD_NUMBER/plugins"
+  export INTELLIJ_HOME="$CWD/.cache/intellij/$IJ_BUILD_NUMBER/idea-dist"
+  export OSS_PANTS_HOME="$CWD/.cache/pants"
+  export JDK_LIBS_HOME="$CWD/.cache/jdk-libs"
 }
