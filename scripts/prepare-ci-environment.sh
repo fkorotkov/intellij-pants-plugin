16ca58a5a02640ff707d77e0f36d209cda987fbd
fkorotkov <fkorotkov@twitter.com>
2015-05-17 17:22:47 -0700

    fixed env vars

--- scripts/prepare-ci-environment.sh
+++ scripts/prepare-ci-environment.sh
@@ -2,6 +2,9 @@
 
 prepare_ci_env() {
   CWD=$(pwd)
+  export IJ_VERSION="14.1"
+  export IJ_BUILD_NUMBER="141.177"
+
   export INTELLIJ_PLUGINS_HOME="$CWD/.cache/intellij/$IJ_BUILD_NUMBER/plugins"
   export INTELLIJ_HOME="$CWD/.cache/intellij/$IJ_BUILD_NUMBER/idea-dist"
   export OSS_PANTS_HOME="$CWD/.cache/pants"
